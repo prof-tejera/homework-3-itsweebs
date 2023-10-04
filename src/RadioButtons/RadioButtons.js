b0f5dcdc47dacc73cda8442191341ec7d91f85fb
itsweebs <90012218+itsweebs@users.noreply.github.com>
2023-10-04 16:15:26 -0400

    Radio buttons component created

--- src/RadioButtons/RadioButtons.js
+++ src/RadioButtons/RadioButtons.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import "./RadioButtons.css";
 
 const RadioButtons = () => {
-  return null;
+  const [activeType, setActiveType] = useState(null);
+
+  const handleClick = (type) => {
+    if (type === activeType) {
+      setActiveType(null);
+    } else {
+      setActiveType(type);
+    }
+  };
+
+  return (
+    <div>
+      <input type="radio"
+        checked={activeType === "Apple"}
+        onChange={() => handleClick("Apple")}
+      />
+      <label>Apple</label>
+    <input type="radio"
+      checked={activeType === "Orange"}
+      onChange={() => handleClick("Orange")}
+    />
+    <label>Orange</label>
+    <input type="radio"
+      checked={activeType === "Banana"}
+      onChange={() => handleClick("Banana")}
+    />
+    <label>Banana</label>
+  </div>
+  );
 };
 
 export default RadioButtons;
