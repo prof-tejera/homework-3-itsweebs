b2ef0366816dc42f14df78abf4aeb49feb20f2dd
itsweebs <90012218+itsweebs@users.noreply.github.com>
2023-10-05 00:40:42 -0400

    created pager component

--- src/Pager/Pager.js
+++ src/Pager/Pager.js
@@ -1,7 +1,48 @@
+import React, { useState } from "react";
 import "./Pager.css";
 
 const Pager = () => {
-  return null;
+  const [activePage, setActivePage] = useState(null);
+
+  const handleClick = (type) => {
+    setActivePage(type);
+  };
+
+  const pageNumbers = () => {
+    const numbers = [];
+
+    for (let i = 1; i <= 10; i++) {
+      numbers.push(
+        <button
+          key={i}
+          active={activePage === (i)}
+          onClick={() => handleClick(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    return numbers;
+  };
+
+  return (
+    <div className="pager">
+      <button
+        onClick={() => handleClick(activePage - 1)}
+        disabled={activePage === 1}
+      >
+        Previous
+      </button>
+      {pageNumbers()}
+      <button
+        onClick={() => handleClick(activePage + 1)}
+        disabled={activePage === 10}
+      >
+        Next
+      </button>
+    </div>
+  );
 };
 
 export default Pager;
